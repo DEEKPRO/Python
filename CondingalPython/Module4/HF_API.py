33a0105d1b5d37bc6d7fef79ddf10b77b809be7e
DEEKPRO <deekshithparitala@gmail.com>
2025-12-30 18:41:13 +1030

    HF_API

---
+++ CondingalPython/Module4/HF_API.py
@@ -0,0 +1,16 @@
+import requests
+# Label 0 is Negative
+# Label 1 is Neutral
+# Label 2 is Positive
+api = "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"
+def classify_text(text):
+    api_url = 'https://router.huggingface.co/hf-inference/models/cardiffnlp/twitter-roberta-base-sentiment'
+    header = {"Authorization":f"Bearer {api}"}
+    payload = {"inputs":text}
+    response = requests.post(api_url, headers=header, json=payload)
+    return response.json()
+
+if __name__ == "__main__":
+    sam_text = input("Enter some text to identify the sentiment: \n")
+    result = classify_text(sam_text)
+    print(result)
