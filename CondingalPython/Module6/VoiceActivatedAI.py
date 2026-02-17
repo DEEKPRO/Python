0d06691b97ab8c2fae953712c864b3847bcd723e
DEEKPRO <deekshithparitala@gmail.com>
2026-02-17 18:39:46 +1030

    VoiceActivatedAI

---
+++ CondingalPython/Module6/VoiceActivatedAI.py
@@ -0,0 +1,54 @@
+import speech_recognition as sr
+import pyttsx3
+from datetime import datetime
+
+def speak(text):
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 150)
+    engine.say(text)
+    engine.runAndWait()
+
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Speak now...")
+        audio = r.listen(source)
+
+        try:
+            command = r.recognize_google(audio)
+            print(f"✅ You said: {command}")
+            return command.lower()
+        except sr.UnknownValueError:
+            print("⭕ Could not understand")
+        
+        except sr.RequestError as e:
+            print(f"⭕ API error: {e}")
+    return ""
+
+def respond_to_command(command):
+    if "hello" in command:
+        speak("Hi there! How can I help you today? ")
+    
+    elif "your name" in command:
+        speak("I am your Python voice assistant")
+    
+    elif "time" in command:
+        now = datetime.now().strftime("%H:%A")
+        speak(f"The time is {now}")
+    
+    elif "exit" in command:
+        speak("Goodbye!")
+        return False
+    else:
+        speak("I'm not sure how to help with that.")
+    
+    return True
+
+def main():
+    speak("Voice assistant activated. Say something!")
+    while True:
+        command = get_audio()
+        if command and not respond_to_command(command):
+            break
+
+main()
