f70bb960a5f759b2d8fcf22f44109fc635f1bf31
DEEKPRO <deekshithparitala@gmail.com>
2026-05-23 13:41:26 +0930

    work

--- DeepLearning/Module2/Lesson5/Polygon.py
+++ DeepLearning/Module2/Lesson5/Polygon.py
@@ -1,20 +1,46 @@
-class polygon:
-    def __init__(self,area,perimeter):
-        self.area = area
-        self.perimeter = self.perimeter
+class Shape:
     def area(self):
-        square = side * 4
-    def perimeter(self):
         pass
 
-class square(polygon):
-    side = int(input())
-    def __init__(self, area, perimeter):
-        super().__init__(area, perimeter)
-    
-class rectangle(polygon):
-    pass
-class triangle(polygon):
-    pass
-class circle(polygon):
-    pass
+class Square(Shape):
+    def __init__(self, side):
+        side = int(side)
+        self.side = side
+    def area(self):
+        return self.side ** 2
+
+class Triangle(Shape):
+    def __init__(self, base, height):
+        base = int(base)
+        height = int(height)
+        self.base = base
+        self.height = height
+
+    def area(self):
+        return 0.5 * self.base * self.height
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        length = int(length)
+        width = int(width)
+        self.length = length
+        self.width = width
+    def area(self):
+        return self.length * self.width
+
+class Circle(Shape):
+    def __init__(self, radius):
+        radius = int(radius)
+        self.radius = radius
+    def area(self):
+        return 22/7 * (self.radius ** 2)
+
+side = input("Side of Square: ")
+length = input("Length of Rectangle: ")
+width = input("Width of Rectangle: ")
+height = input("Height of Triangle: ")
+base = input("Base of Triangle: ")
+radius = input("Radius of Circle: ")
+
+for shape in (Square(side), Triangle(base, height), Rectangle(length, width), Circle(radius)):
+    print(shape.area())
